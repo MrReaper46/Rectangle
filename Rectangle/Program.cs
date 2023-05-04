59a0ea91753432931f7101976493bb8855290cc1
Van Lam <lamvanr@gmail.com>
2023-05-04 11:29:56 +0700

    Add project files.

---
+++ Rectangle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the width:");
+            double width = Double.Parse(Console.ReadLine());
+            Console.Write("Enter the height:");
+            double height = Double.Parse(Console.ReadLine());
+            Console.WriteLine("========================");
+
+            Rectangle rectangle = new Rectangle(width, height);
+            Console.WriteLine("Your Rectangle info: \n" + rectangle.Display());
+            Console.WriteLine("========================");
+
+            Console.WriteLine("Perimeter of the Rectangle: " + rectangle.GetPerimeter());
+            Console.WriteLine("Area of the Rectangle: " + rectangle.GetArea());
+
+        }
+    }
+}
